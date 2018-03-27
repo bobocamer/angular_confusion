e9e0ba1ff420fe3a47e918085e95452fe17e8978
Boaz Njintchoum <boaz.na@gmail.com>
2018-03-26 19:23:57 -0700

    Promise Part 1

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -6,15 +6,15 @@ export class LeaderService {
 
   constructor() { }
 
-  getLeaders(): leader[] {
-    return LEADERS;
+  getLeaders(): Promise<leader[]> {
+    return Promise.resolve(LEADERS);
   }
 
-  getLeader(id: number): leader {
-    return LEADERS.filter((leader) => (leader.id === id ))[0];
+  getLeader(id: number): Promise<leader> {
+    return Promise.resolve(LEADERS.filter((leader) => (leader.id === id ))[0]);
   }
   
-  getFeaturedLeader(): leader {
-    return LEADERS.filter((leader) => leader.featured)[0];
+  getFeaturedLeader(): Promise<leader> {
+    return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
   }
 }
